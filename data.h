60a0c797864ea90370a12f725ae389ca90215cb6
JiroKristoferTipan <jttipan1@up.edu.ph>
2026-04-19 21:07:32 +0800

    feat: made func to check solution and changed format of subsets to end with '-1' and have the same length

--- data.h
+++ data.h
@@ -9,12 +9,42 @@
 //EDIT NALANG TO TEST DIFFERENT CASES
 static int mainset[mainset_count] = {1, 2, 3, 5, 8};
 static int subsets[subset_count][item_count] = {
-    {1, 2},
-    {3, 5},
-    {1},
-    {2, 3},
-    {5, 8},
-    {8}
+    {1, 2, -1, 0, 0},
+    {3, 5, -1, 0, 0},
+    {1, -1, 0, 0, 0},
+    {2, 1, 8, -1, 0},
+    {5, 8, -1, 0, 0},
+    {8, -1, 0, 0, 0}
 };
+
+void print_sol(int result, int *state) {
+    if (result == 9999) {
+        printf("No solution found.\n");
+    } else {
+        printf("Minimum Subsets Needed: %d\n", result);
+        printf("Indices of subsets used: ");
+        
+        for (int i = 0; i < result; i++) {
+            printf("%d ", state[i]);
+        }
+        printf("\n\n");
+        
+        // Optional: Print the actual values in those subsets
+        int count = 0;
+        for (int i = 0; i < result; i++) {
+            // Get the subset index from our result array
+            int subset_idx = state[i]; 
+            
+            printf("Subset %d contains: ", subset_idx);
+            for (int j = 0; j < item_count; j++) {
+                // Stop printing if we hit the -1 sentinel
+                if (subsets[subset_idx][j] == -1) break;
+                printf("%d ", subsets[subset_idx][j]);
+            }
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
 
 #endif
--- main.c
+++ main.c
@@ -5,22 +5,30 @@
 #include <time.h>
 
 
-int main() {   
+int main() {
+    /*
+    time - ilan secs to run
+    result - ilan subsets ginamit
+    selected - checker lang, it should be all 1s if tama
+    state - which subsets are selected, 1 if selected, 0 if not
+    */
     int time_bruteforce, time_dynamic, result_brute, result_dynamic; 
-    int bruteforce_state[mainset_count], dynamic_state[mainset_count];
+    int bruteforce_selected[mainset_count], bruteforce_state[subset_count], dynamic_selected[mainset_count], dynamic_state[subset_count] = {0};
 
 
     //GET TIME FOR BRUTE FORCE
     time_bruteforce = clock();
-    result_brute = solve_brute(bruteforce_state, mainset_count, subset_count, item_count, mainset, subsets);
+    result_brute = solve_brute(bruteforce_selected, mainset_count, subset_count, item_count, mainset, subsets, bruteforce_state);
     time_bruteforce = clock() - time_bruteforce;
     printf("Time taken (brute force): %f seconds\n", ((double)time_bruteforce) / CLOCKS_PER_SEC);
+    print_sol(result_brute, bruteforce_state);
 
     //GET TIME FOR DYNAMIC PROGRAMMING
     time_dynamic = clock();
-    result_dynamic = solve_dynamic(dynamic_state, mainset_count, subset_count, item_count, mainset, subsets);
+    result_dynamic = solve_dynamic(dynamic_state, mainset_count, subset_count, item_count, mainset, subsets, dynamic_selected);
     time_dynamic = clock() - time_dynamic;
     printf("Time taken (dynamic programming): %f seconds\n", ((double)time_dynamic) / CLOCKS_PER_SEC);
+    // print_sol(result_dynamic, dynamic_state);
 
     return 0;
 }
